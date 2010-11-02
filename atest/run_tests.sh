1779f7287236f9e9f6dd54a5a3bcc28045937c25
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-02 07:04:07 +0000

    use library from sources

--- atest/run_tests.sh
+++ atest/run_tests.sh
@@ -1,5 +1,5 @@
 OUTDIR=results
 
-pybot -d $OUTDIR -l pybot_log -r pybot_report -o pybot_output $* atest 
-jybot -d $OUTDIR -l jybot_log -r jybot_report -o jybot_output \
+pybot -d $OUTDIR -l pybot_log -r pybot_report -o pybot_output -P src $* atest 
+jybot -d $OUTDIR -l jybot_log -r jybot_report -o jybot_output -P src \
  -P lib/trilead-ssh2-build213.jar $* atest
