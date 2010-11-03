09cc99d62688985e3f0952878db2035eceeae15a
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-03 06:57:57 +0000

    Got rid of DataError

--- src/SSHLibrary/connectioncache.py
+++ src/SSHLibrary/connectioncache.py
@@ -12,8 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-
-from robot.errors import DataError
 from robot.utils.normalizing import NormalizedDict
 
 
@@ -24,7 +22,7 @@ class ConnectionCache:
     This cache stores connections and allows switching between them using
     generated indexes or user given aliases. Can be used for example by web
     testing libraries where there's need for multiple concurrent connections.
-    
+
     Note that in most cases there should be only one instance of this class but
     this is not enforced.
     """
@@ -38,8 +36,8 @@ class ConnectionCache:
 
     def register(self, connection, alias=None):
         """Registers given connection with optional alias and returns its index.
-        
-        Given connection is set to be the current connection. Alias must be 
+
+        Given connection is set to be the current connection. Alias must be
         a string. The index of the first connection after initialization or
         close_all or empty_cache is 1, second is 2, etc.
         """
@@ -49,10 +47,10 @@ class ConnectionCache:
         if isinstance(alias, basestring):
             self._aliases[alias] = self.current_index
         return self.current_index
-    
+
     def switch(self, index_or_alias):
         """Switches to the connection specified by given index or alias.
-        
+
         If alias is given it must be a string. Indexes can be either integers
         or strings that can be converted into integer. Raises a DataError
         if no connection with given index or alias found.
@@ -60,38 +58,38 @@ class ConnectionCache:
         try:
             index = self._get_index(index_or_alias)
         except ValueError:
-            raise DataError("Non-existing index or alias '%s'" % index_or_alias)
+            raise ValueError("Non-existing index or alias '%s'" % index_or_alias)
         self.current = self._connections[index-1]
         self.current_index = index
         return self.current
 
     def close_all(self, closer_method='close'):
         """Closes connections using given closer method and empties cache.
-        
+
         If simply calling the closer method is not adequate for closing
         connections, clients should close connections themselves and use
-        empty_cache afterwards.         
+        empty_cache afterwards.
         """
         for conn in self._connections:
             getattr(conn, closer_method)()
         self.empty_cache()
         return self.current
-                
+
     def empty_cache(self):
         """Empties the connections cache.
-        
+
         Indexes of new connections starts from 1 after this."""
         self.current = self._no_current
         self.current_index = None
         self._connections = []
         self._aliases = NormalizedDict()
-        
+
     def _get_index(self, index_or_alias):
         try:
             return self._resolve_alias(index_or_alias)
         except ValueError:
             return self._resolve_index(index_or_alias)
-    
+
     def _resolve_alias(self, alias):
         if isinstance(alias, basestring):
             try:
@@ -108,14 +106,14 @@ class ConnectionCache:
 
 
 class _NoConnection:
-    
+
     def __init__(self, msg):
         self._msg = msg
 
     def __getattr__(self, name):
         if name.startswith('__') and name.endswith('__'):
             raise AttributeError
-        raise DataError(self._msg)
+        raise RuntimeError(self._msg)
 
     def __nonzero__(self):
         return False
