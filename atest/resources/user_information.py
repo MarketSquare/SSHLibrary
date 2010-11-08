e74207ff18ae4bdbb38b75b385f6220f2836ede8
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-08 07:16:55 +0000

    Split public key tests in own suite

--- atest/resources/user_information.py
+++ atest/resources/user_information.py
@@ -7,9 +7,3 @@ USERNAME = "test" #user"
 PASSWORD = "test"
 PROMPT = "$"
 
-PUBKEY_USERNAME = 'testkey'
-PUBKEY_PASSWORD = 'testkey'
-PUBKEY_FILE = os.path.join(basedir, 'id_rsa')
-INVALID_PUBKEY_USERNAME = 'invalid_key_username'
-INVALID_PUBKEY_FILE = os.path.join(basedir, 'invalid_rsa')
-INACCESSIBLE_PUBKEY_FILE = os.path.join(basedir, 'id_rsa_inaccessible')
