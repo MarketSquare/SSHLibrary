e78540fc9e0825abfd907abb2085cf6cfb0ce922
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-08 07:26:48 +0000

    cleanup

--- atest/resources/user_information.py
+++ atest/resources/user_information.py
@@ -1,9 +1,4 @@
-import os
-
-basedir = os.path.dirname(__file__)
-
-HOST = "localhost" #"172.21.106.53"
-USERNAME = "test" #user"
+HOST = "localhost"
+USERNAME = "test"
 PASSWORD = "test"
 PROMPT = "$"
-
