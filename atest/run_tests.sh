60ea6cbf99a0ef39022f427ad03b3b3e6e4c207f
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-08 07:10:57 +0000

    set critical tags

--- atest/run_tests.sh
+++ atest/run_tests.sh
@@ -1,5 +1,5 @@
 OUTDIR=results
+COMMON_FLAGS="-d $OUTDIR -P src -P lib/trilead-ssh2-build213.jar --critical regression"
 
-pybot -d $OUTDIR -l pybot_log -r pybot_report -o pybot_output -P src $* atest 
-jybot -d $OUTDIR -l jybot_log -r jybot_report -o jybot_output -P src \
- -P lib/trilead-ssh2-build213.jar $* atest
+pybot $COMMON_FLAGS -l pybot_log -r pybot_report -o pybot_output $* atest 
+jybot $COMMON_FLAGS -l jybot_log -r jybot_report -o jybot_output $* atest
