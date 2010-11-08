2bfb588e0c184e8dd2833115ecfd287abcaa7d10
Janne Härkönen <janne.t.harkonen@gmail.com>
2010-11-08 07:51:21 +0000

    update contact information

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ def main():
           version      =  SSHLibrary.__version__,
           description  = 'Test Library for Robot Framework enabling SSH',
           author       = 'Robot Framework Developers',
-          author_email = 'robotframework-sshlibrary@groups.google.com',
+          author_email = 'robotframework-devel@groups.google.com',
           url          = 'http://code.google.com/p/robotframework-sshlibrary/',
           package_dir  = { '' : 'src'},
           packages     = ['SSHLibrary']
           )
-        
+
 
 if __name__ == "__main__":
     main()
