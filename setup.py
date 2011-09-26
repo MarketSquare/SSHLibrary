37e8426ea53be045f165b3b9551f1d24a7bb59b6
Janne Härkönen <janne.t.harkonen@gmail.com>
2011-09-26 05:07:51 +0000

    setup.py: Metadata for Pypi

--- setup.py
+++ setup.py
@@ -5,18 +5,24 @@ import sys
 sys.path.insert(0, 'src')
 import SSHLibrary
 
+CLASSIFIERS = """
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
 
-def main():
-    setup(name         = 'SSHLibrary',
-          version      =  SSHLibrary.__version__,
-          description  = 'Test Library for Robot Framework enabling SSH',
-          author       = 'Robot Framework Developers',
-          author_email = 'robotframework-devel@groups.google.com',
-          url          = 'http://code.google.com/p/robotframework-sshlibrary/',
-          package_dir  = { '' : 'src'},
-          packages     = ['SSHLibrary']
-          )
-
-
-if __name__ == "__main__":
-    main()
+setup(name         = 'robotframework-sshlibrary',
+      version      =  SSHLibrary.__version__,
+      description  = 'Test Library for Robot Framework enabling SSH',
+      author       = 'Robot Framework Developers',
+      author_email = 'robotframework-devel@groups.google.com',
+      url          = 'http://code.google.com/p/robotframework-sshlibrary/',
+      license      = 'Apache License 2.0',
+      keywords     = 'robotframework testing testautomation ssh',
+      platforms    = 'any',
+      classifiers  = CLASSIFIERS.splitlines(),
+      package_dir  = { '' : 'src'},
+      packages     = ['SSHLibrary']
+      )
