34b7ef9ac73a0953462fe919550ff4214c8d5aa2
Janne Härkönen <janne.t.harkonen@gmail.com>
2012-08-14 05:11:37 +0000

    run_tests: fix syntax errors

--- atest/run_tests.sh
+++ atest/run_tests.sh
@@ -1,12 +1,12 @@
 OUTDIR=results
 PYBOT=
 JYBOT=
-if [ $1 = "pybot" ] ; then
+if [ "$1" = "pybot" ]; then
     PYBOT=true;
-    shift
-elif [ $1 = "jybot" ] ; then
+    shift;
+elif [ "$1" = "jybot" ]; then
     JYBOT=true;
-    shift
+    shift;
 else
     PYBOT=true
     JYBOT=true
@@ -14,10 +14,10 @@ fi
 
 COMMON_FLAGS="-d $OUTDIR -l NONE -r NONE -P src -P lib/trilead-ssh2-build213.jar --critical regression"
 
-if [ $PYBOT != "" ]; then
+if [ "$PYBOT" != "" ] ; then
     pybot $COMMON_FLAGS -o pybot_output $* atest
 fi
-if [ $JYBOT != "" ]; then
+if [ "$JYBOT" != "" ] ; then
     jybot $COMMON_FLAGS -o jybot_output $* atest
 fi
 rebot -d $OUTDIR $OUTDIR/*.xml
