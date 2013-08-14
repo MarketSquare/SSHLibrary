ab4d4e13b8578d1a7e6e02eaeadc5907e6a4a02a
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-08-14 16:53:49 +0300

    Setup.py to handle dependencies if setuptools are installed

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+    kw = {
+        'install_requires': 'paramiko >= 1.8.0',
+    }
+except ImportError:
+    from distutils.core import setup
+    kw = {}
+
 from os.path import abspath, dirname, join
+
 execfile(join(dirname(abspath(__file__)), 'src', 'SSHLibrary', 'version.py'))
+
+# Maximum width in Windows installer seems to be 70 characters -------|
+DESCRIPTION = """
+This is a Robot Framework test library for testing SSH and SFTP.
+
+Required packages:
+    paramiko
+"""[1:-1]
 
 CLASSIFIERS = """
 Development Status :: 5 - Production/Stable
@@ -15,14 +32,16 @@ Topic :: Software Development :: Testing
 setup(
     name='robotframework-sshlibrary',
     version=VERSION, # resolved with execfile
-    description='Test Library for Robot Framework enabling SSH',
+    description='Robot Framework test library for SSH and SFTP',
+    long_description=DESCRIPTION,
     author='Robot Framework Developers',
     author_email='robotframework-devel@groups.google.com',
     url='http://code.google.com/p/robotframework-sshlibrary/',
     license='Apache License 2.0',
     keywords='robotframework testing testautomation ssh',
-    platforms='any',
+    platforms='Posix, MacOS X, Windows',
     classifiers=CLASSIFIERS.splitlines(),
     package_dir={'': 'src'},
-    packages=['SSHLibrary']
+    packages=['SSHLibrary'],
+    **kw
 )
