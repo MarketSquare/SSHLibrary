ab3a583cad1aa150702dcfeddd2e1bbcab4168e8
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-09-09 17:16:46 +0300

    Atest runner: Fixed the JAR path if already inside 'atest' directory

--- atest/run_atests.sh
+++ atest/run_atests.sh
@@ -2,18 +2,21 @@
 if [ "$(basename $PWD)" != "atest" ]; then
     OUTPUT_ROOT="atest/"
     COMMON_ARGS="-P src"
+    JAR_ROOT="lib/"
 else
     OUTPUT_ROOT=""
     COMMON_ARGS="-P ../src"
+    JAR_ROOT="../lib/"
 fi
 
 OUTPUT_ROOT+="results"
+JAR_ROOT+="trilead-ssh2-build213.jar"
 
 if [ "$1" == "jython" ]; then
     shift
     OUTPUT_DIR="$OUTPUT_ROOT/jython"
     rm -rf $OUTPUT_DIR 2> /dev/null
-    jybot $COMMON_ARGS -P lib/trilead-ssh2-build213.jar -d $OUTPUT_DIR -i jybot $*
+    jybot $COMMON_ARGS -P $JAR_ROOT -d $OUTPUT_DIR -i jybot $*
 elif [ "$1" == "python" ]; then
     shift
     OUTPUT_DIR="$OUTPUT_ROOT/python"
