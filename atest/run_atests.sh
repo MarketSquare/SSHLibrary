279cc54042e86aab85ce5527beb3672b020ce9b0
Anssi Syrjäsalo <anssi.syrjasalo@eficode.com>
2013-09-13 13:11:02 +0300

    Refactored atest runnner

--- atest/run_atests.sh
+++ atest/run_atests.sh
@@ -1,28 +1,31 @@
+#!/bin/bash
 
-if [ "$(basename $PWD)" != "atest" ]; then
-    OUTPUT_ROOT="atest/"
-    COMMON_ARGS="-P src"
-    JAR_ROOT="lib/"
-else
-    OUTPUT_ROOT=""
+function silent_remove_dir {
+    rm -rf $1 2> /dev/null
+}
+
+if [ "$(basename $PWD)" == "atest" ]; then
+    OUTPUT_ROOT="results"
     COMMON_ARGS="-P ../src"
-    JAR_ROOT="../lib/"
+    JAR_PATH="../lib/"
+else
+    OUTPUT_ROOT="atest/results"
+    COMMON_ARGS="-P src"
+    JAR_PATH="lib/"
 fi
 
-OUTPUT_ROOT+="results"
-JAR_ROOT+="trilead-ssh2-build213.jar"
-
-if [ "$1" == "jython" ]; then
-    shift
-    OUTPUT_DIR="$OUTPUT_ROOT/jython"
-    rm -rf $OUTPUT_DIR 2> /dev/null
-    jybot $COMMON_ARGS -P $JAR_ROOT -d $OUTPUT_DIR -i jybot $*
-elif [ "$1" == "python" ]; then
+if [ "$1" == "python" ]; then
     shift
     OUTPUT_DIR="$OUTPUT_ROOT/python"
-    rm -rf $OUTPUT_DIR 2> /dev/null
+    silent_remove_dir $OUTPUT_DIR
     pybot $COMMON_ARGS -d $OUTPUT_DIR -i pybot $*
+elif [ "$1" == "jython" ]; then
+    shift
+    JAR_PATH+="trilead-ssh2-build213.jar"
+    OUTPUT_DIR="$OUTPUT_ROOT/jython"
+    silent_remove_dir $OUTPUT_DIR
+    jybot $COMMON_ARGS -P $JAR_PATH -d $OUTPUT_DIR -i jybot $*
 else
-    echo "Usage: atest/run_atests.sh (python|jython) <target>"
+    echo "usage: $0 (python|jython) <test_suite_path>"
     exit 1
 fi
