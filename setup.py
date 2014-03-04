4840fee440509c0b2020e38da8a2793ee95b7432
Mikko Korpela <mikko.korpela@gmail.com>
2014-03-04 16:01:14 +0200

    setup.py: Use README.rst for long description

--- setup.py
+++ setup.py
@@ -17,24 +17,10 @@ except ImportError:
 
 from os.path import abspath, dirname, join
 
-execfile(join(dirname(abspath(__file__)), 'src', 'SSHLibrary', 'version.py'))
-
-# Maximum width in Windows installer seems to be 70 characters -------|
-DESCRIPTION = """
-This is a Robot Framework test library for testing SSH and SFTP.
-
-The library has the following main usages:
-- Executing commands on remote, either with blocking or non-blocking
-  behaviour.
-- Writing and reading in an interactive session.
-- Transferring files and directories over SFTP.
-- Ensuring that files or directories exist on remote.
-
-Required packages:
-    robotframework
-    paramiko
-"""[1:-1]
-
+CURDIR = dirname(abspath(__file__))
+execfile(join(CURDIR, 'src', 'SSHLibrary', 'version.py'))
+with open(join(CURDIR, 'README.rst')) as readme:
+    README = readme.read()
 CLASSIFIERS = """
 Development Status :: 5 - Production/Stable
 License :: OSI Approved :: Apache Software License
@@ -47,9 +33,9 @@ setup(
     name='robotframework-sshlibrary',
     version=VERSION, # resolved with execfile
     description='Robot Framework test library for SSH and SFTP',
-    long_description=DESCRIPTION,
+    long_description=README,
     author='Robot Framework Developers',
-    author_email='robotframework-devel@groups.google.com',
+    author_email='robotframework@gmail.com',
     url='http://code.google.com/p/robotframework-sshlibrary/',
     license='Apache License 2.0',
     keywords='robotframework testing testautomation ssh sftp',
