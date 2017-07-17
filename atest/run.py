3313f4442b30fce96829a29fb235a617cd982412
mihaiparvu <mihaiparvu90@gmail.com>
2017-07-17 14:58:02 +0300

    fixed run.py

--- atest/run.py
+++ atest/run.py
@@ -16,10 +16,9 @@
 import sys
 import os
 
-from os.path import abspath, dirname, exists, join, normpath
+from os.path import abspath, dirname, join
 from robot import run_cli, rebot
 from robotstatuschecker import process_output
-
 
 CURDIR = dirname(abspath(__file__))
 OUTPUT_ROOT = join(CURDIR, 'results')
@@ -35,40 +34,40 @@ COMMON_OPTS = ('--log', 'NONE', '--report', 'NONE')
 def atests(*opts):
     if os.name == 'java':
         os_includes = get_os_includes(os._name)
-        jython(*(os_includes+opts))
+        jython(*(os_includes + opts))
         process_output(join(OUTPUT_JYTHON, 'output.xml'))
         return rebot(join(OUTPUT_JYTHON, 'output.xml'), outputdir=OUTPUT_JYTHON)
     else:
         os_includes = get_os_includes(os.name)
-        python(*(os_includes+opts))
+        python(*(os_includes + opts))
         process_output(join(OUTPUT_PYTHON, 'output.xml'))
         return rebot(join(OUTPUT_PYTHON, 'output.xml'), outputdir=OUTPUT_PYTHON)
 
 
 def get_os_includes(operating_system):
     if operating_system == 'nt':
-        return ('--include', 'windows',
-                '--exclude', 'linux')
-    return ('--include', 'linux',
-            '--exclude', 'windows')
+        return '--exclude', 'linux'
+    return '--exclude', 'windows'
 
 
 def python(*opts):
     try:
         run_cli(['--outputdir', OUTPUT_PYTHON,
-                '--include', 'pybot']
+                 '--include', 'pybot']
                 + list(COMMON_OPTS + opts))
     except SystemExit:
         pass
 
+
 def jython(*opts):
     try:
         run_cli(['--outputdir', OUTPUT_JYTHON,
-                '--pythonpath', JAR_PATH,
-                '--include', 'jybot']
+                 '--pythonpath', JAR_PATH,
+                 '--include', 'jybot']
                 + list(COMMON_OPTS + opts))
     except SystemExit:
         pass
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 or '--help' in sys.argv:
